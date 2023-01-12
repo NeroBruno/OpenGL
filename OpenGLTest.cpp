c4857cb20ea2968a534c78c3da93bf59ea8fc905
BrunoCouto <fwcouto10@gmail.com>
2023-01-12 18:23:41 +0000

    OpenGL Hello Window

--- OpenGLTest.cpp
+++ OpenGLTest.cpp
@@ -1,5 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main()
 {
@@ -12,5 +15,39 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
+	// Creating window object
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGLTest", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW Window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Initialize GLAD to load OpenGL function pointers
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "FAiled to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// rendering loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// call events and swap the buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Clear all resources alocated by GLFW
+	glfwTerminate();
 	return 0;
+}
+
+// Whenever the window's size is changed this callback function is executed
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
 }
